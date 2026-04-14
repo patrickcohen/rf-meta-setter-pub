31d523f465f2bf6c224b0a0ccecc1bcbe0f8aa7d
Patrick Cohen <patrick@redfactory.nl>
2026-04-14 10:54:47 +0200

    v1.2: use update_option to remove self from active plugins

--- rf-meta-setter.php
+++ rf-meta-setter.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: RF Meta Setter
- * Version: 1.1
+ * Version: 1.2
  */
-add_action('init', function() {
-    global $wpdb;
-    // Remove self from active_plugins
-    $active = get_option('active_plugins', []);
-    $self = 'rf-meta-setter-pub-main/rf-meta-setter.php';
-    $active = array_filter($active, fn($p) => $p !== $self);
-    $wpdb->update($wpdb->options, ['option_value' => serialize(array_values($active))], ['option_name' => 'active_plugins']);
-    wp_cache_delete('active_plugins', 'options');
-});
+// Self-remove from active plugins list
+add_action('plugins_loaded', function() {
+    $self = plugin_basename(__FILE__);
+    $active = (array) get_option('active_plugins', []);
+    if (in_array($self, $active)) {
+        $active = array_values(array_diff($active, [$self]));
+        update_option('active_plugins', $active);
+    }
+}, 1);
